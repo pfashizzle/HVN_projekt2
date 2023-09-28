dbff788d1aba3e9da8faa45cbb5a971d2d936996
pfashizzle <115485563+pfashizzle@users.noreply.github.com>
2023-09-28 09:34:14 +0200

    Changed Language

--- projekt.py
+++ projekt.py
@@ -5,36 +5,33 @@ import ujson
 import utime
 
 print("Hello")
-# Läs konfigurationsfil
+# Read configuration file
 with open('config.json', 'r') as config_file:
     config = ujson.load(config_file)
 
-# Konfigurera UART
+# Configure UART
 # uart = machine.UART(config["uart_port"], baudrate=config["baud_rate"])
 
 print(config["sensor_pin"])
 
-# Skapa en OneWire-bus och DS18X20-sensor
+# Create a OneWire bus and DS18X20 sensor
 ds_pin = machine.Pin(config["sensor_pin"])
 ds_bus = onewire.OneWire(ds_pin)
 ds_sensor = ds18x20.DS18X20(ds_bus)
 
-
 while True:
-    # Starta mätning
+    # Start temperature measurement
     ds_sensor.convert_temp()
 
-    # Vänta på att mätningen ska bli klar
+    # Wait for the measurement to complete
     utime.sleep_ms(900)
 
-    # Läs temperaturen från sensorn
+    # Read the temperature from the sensor
     temperature = ds_sensor.read_temp(ds_sensor.scan()[0])
 
-    # Skicka temperaturen över UART
-    #uart.write("Sensor ID: {}\nTemperature: {:.2f}°C\n".format(config["sensor_id"], temperature))
+    # Send the temperature over UART
+    # uart.write("Sensor ID: {}\nTemperature: {:.2f}°C\n".format(config["sensor_id"], temperature))
     print("Sensor ID: {}\nTemperature: {:.2f}°C\n".format(config["sensor_id"], temperature))
 
-
-    # Vänta enligt mätningsintervallet
-    #utime.sleep(config["measurement_interval"])
-
+    # Wait according to the measurement interval
+    # utime.sleep(config["measurement_interval"])
